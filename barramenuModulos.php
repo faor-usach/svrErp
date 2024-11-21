41e76be3ae0da983ec78562d6e82e17b9ce84a34
FAOR <francisco.olivares.r@usach.cl>
2024-11-21 19:08:56 -0300

    segundo

---
+++ barramenuModulos.php
@@ -0,0 +1,26 @@
+
+    	<div id="icons1">
+			<div class="icons img">
+				<div id="icons">
+    				<a href="/plataformaErp.php" target="_self" title="Menú Plataforma">
+						<img src="/gastos/imagenes/Menu.png" border="0" width="32" height="32"></a>
+
+						
+    				<a href="/gastos/plataformaintranet.php" target="_self" title="Módulo de Gastos">
+						<img src="/gastos/imagenes/group_48.png" border="0" width="32" height="32"></a>
+
+    				<a href="/personal/sueldos.php" target="_self" title="Módulo de Sueldos y Personal">
+						<img src="/gastos/imagenes/subst_student.png" border="0" width="32" height="32"></a>
+
+    				<a href="/facturacion/plataformaFacturas.php" target="_self" title="Módulo de Solicitudes de Facturación">
+						<img src="/gastos/imagenes/crear_certificado.png" border="0" width="32" height="32"></a>
+
+					<img src="/gastos/imagenes/r_x.png"  width="32" height="32">
+
+    				<a href="/gastos/cerrarsesion.php" target="_self" title="Cerrar Sesión">
+						<img src="/gastos/imagenes/preview_exit_32.png" border="0" width="32" height="32"></a>
+						
+    			</div>
+			</div>
+		</div>
+
